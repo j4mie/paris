23f954884995c5d6b576109e83d3fa6ff02f2db5
Simon Holywell <treffynnon@php.net>
2014-01-21 11:33:22 +0000

    Remove Durham properly

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Paris'
-copyright = u'2014, Jamie Matthews and Durham Hale'
+copyright = u'2014, Jamie Matthews and Simon Holywell'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
